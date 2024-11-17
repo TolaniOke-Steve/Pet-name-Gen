c4e85bbf334b80e2d16e242212d47f96a6efe978
TOLANI OKE-STEVE <emmasam1247@gamil.com>
2024-11-17 16:26:15 -0600

    update

--- Name_gen/Pet_name.sh
+++ Name_gen/Pet_name.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+echo "Enter your pet type"
+read type 
+
 echo "Enter your pet Breed"
+read breed
 
 echo "Enter the type of name you want (Fantasy or Real Names)"
+read name
 
+echo "Your selected options are type:$type breed: $breed name: $name"
+
+declare -a petType
+
+petType=("Dog" "Cat" "Bird" "Fish" "Hamster" "Reptile" "Snake" "Chicken" "Exotic")
+function get_breed($breed){
+    
+}
