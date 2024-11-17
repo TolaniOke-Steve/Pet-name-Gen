6f58fb9f2632b8f042ae483b7e14fbb1021aac68
TOLANI OKE-STEVE <emmasam1247@gamil.com>
2024-11-17 16:53:25 -0600

    updates

--- Name_gen/Pet_name.sh
+++ Name_gen/Pet_name.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
+#inputs
 echo "Enter your pet type"
 read type 
 
 echo "Enter your pet Breed"
 read breed
 
+echo "Enter pet Sex (M/F)"
+read gender
+
 echo "Enter the type of name you want (Fantasy or Real Names)"
 read name
 
-echo "Your selected options are type:$type breed: $breed name: $name"
+echo "Your selected options are type:$type breed: $breed name: $name Sex: $gender"
 
+#arrays for types and names
 declare -a petType=("Dog" "Cat" "Bird" "Fish" "Hamster" "Reptile" "Snake" "Chicken" "Exotic")
 
+declare -a petNameReal_male=("Pete" "Gib" "Negan" "Leroy" "Arnold" "Barney" "Henry" "Kine" "Justin" "Walter" "Alan" "Marcus" "Elez" "Megatron" "Tzeka")
+
+declare -a petNameReal_female=("Beltina" "Admeta" "Celiaj" "Rebbeca" "Angela" "Kim" "Megatron" "Potts" "Pavlina" "Grecia" "Cheakina")
+
+declare -a exotic_names=("Megatron" "Ehnoll" "Nils" "StarScream" "Bumblebee" "DiamondHead" "Goop" "Grey" "Shatterstar" "Pater")
+
+#functions
 get_breed(){
     local breed=$1
     echo "The breed youve entered is: $breed"
