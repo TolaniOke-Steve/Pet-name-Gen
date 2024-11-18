3ae12e0f92330a63333c67b2e8305077182a8d53
TOLANI OKE-STEVE <emmasam1247@gamil.com>
2024-11-17 18:06:03 -0600

    updates

--- Name_gen/Pet_name.sh
+++ Name_gen/Pet_name.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
+#normalizing user inputs to match values in array
+normalize(){
+    local input=$1
+    input=$(echo "$input" | tr '[:upper:]' '[:lower:]' | tr -d ' ')
+    echo "$(echo "$input" | sed -e 's/\b\(.\)/\u\1/g')"
+}
+
+normalize_Gender(){
+    local input=$1
+    input=$(echo "$input"| tr '[:upper:]' '[:lower:]' | tr -d ' ')
+}
+
 #inputs
 echo "Enter your pet type"
 read type 
+norm_type=$(normalize "$type")
 
 echo "Enter pet Sex (M/F)"
 read gender
+norm_Gen=$(normalize_Gender "$gender")
 
-echo "Enter the type of name you want (Fantasy or Real Names)"
+echo "Enter the type of name you want (Fantasy or Real)"
 read name
+norm_name=$(normalize "$name")
 
-echo "Your selected options are type:$type  name: $name Sex: $gender"
+echo "Your selected options are type:$type  nameType: $name Sex: $gender"
 
 #arrays for types and names
 declare -a petType=("Dog" "Cat" "Bird" "Fish" "Hamster" "Reptile" "Snake" "Chicken" "Exotic")
@@ -23,11 +38,9 @@ declare -a Fantasy_names=("Megatron" "Ehnoll" "Nils" "StarScream" "Bumblebee" "D
 
 #functions
 
-#normalizing user inputs to match array
-normalize(){
-    local input=$1
-    input=$(echo"$input" | tr '[:upper:]' '[:lower:]' | tr -d ' ')
-    echo "$(echo "$input" | sed -e 's/\b\(.\)/\u\1/g')"
+get_name(){
+
+    if ($norm_type == )
 }
 
 get_breed(){
